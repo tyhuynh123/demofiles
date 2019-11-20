6767d9057b41331cdc10d59dd2463e35d1c13b5f
tyhuynh123 <tyhuynh123@yahoo.com>
2019-11-20 14:13:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,52 +2,61 @@
 
 session_start();
 
+if( isset($_SESSION['user_id']) ){
+        header("Location: /");
+}
+
 require 'database.php';
 
-if( isset($_SESSION['user_id']) ){
+if(!empty($_POST['email']) && !empty($_POST['password'])):
 
-	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
-	$records->bindParam(':id', $_SESSION['user_id']);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+        $records = $conn->prepare('SELECT id,email,password FROM users WHERE email = :email');
+        $records->bindParam(':email', $_POST['email']);
+        $records->execute();
+        $results = $records->fetch(PDO::FETCH_ASSOC);
 
-	$user = NULL;
+        $message = '';
 
-	if( count($results) > 0){
-		$user = $results;
-	}
+        if(count($results) > 0 && password_verify($_POST['password'], $results['password']) ){
 
-}
+                $_SESSION['user_id'] = $results['id'];
+                 header("Location: upload.php?uploadsuccess");
+        } else {
+                $message = 'Sorry, those credentials do not match';
+        }
+
+endif;
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome to your Web App</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+        <title>Login Below</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-	<div class="header">
-		<a href="/">Demosite Pic Upload</a>
-	</div>
+        <div class="header">
+        <font size="+10">Ty Photo App</font>
+        </div>
 
-	<?php if( !empty($user) ): ?>
+        <?php if(!empty($message)): ?>
+                <p><?= $message ?></p>
+        <?php endif; ?>
 
-		<br />Welcome <?= $user['email']; ?> 
-		<br /><br />You are successfully logged in!
-		<br /><br />
-		<a href="logout.php">Logout?</a>
+        <h1>Login</h1>
+        <span>or <a href="register.php">register here</a></span>
 
-	<?php else: ?>
+        <form action="login.php" method="POST">
 
-		<h1>Please Login or Register</h1>
-		<a href="login.php">Login</a> or
-		<a href="register.php">Register</a>
+                <input type="text" placeholder="Enter your email" name="email">
+                <input type="password" placeholder="and password" name="password">
 
-	<?php endif; ?>
+                <input type="submit">
+
+        </form>
 
 </body>
 </html>
