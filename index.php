47f0d230b96a82a382772362baa7f5c035824fa7
tyhuynh123 <tyhuynh123@yahoo.com>
2019-11-21 15:42:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ endif;
 <body>
 
         <div class="header">
-        <font size="+10">Kari Photo App</font>
+        <font size="+10">Tybo Photo App</font>
         </div>
 
         <?php if(!empty($message)): ?>
